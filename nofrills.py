7c863991ccabd4f0799e8da387f1e555b5aa6ae6
Nicholas Mascioni <68752011+nicholasmascioni@users.noreply.github.com>
2022-08-20 12:51:22 -0400

    Nofrills file

---
+++ nofrills.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.nofrills.ca/search?search-bar=bananas"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
