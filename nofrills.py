8cb412d07fbe7f629aa5c5092302a97477bad1c7
Nicholas Mascioni <68752011+nicholasmascioni@users.noreply.github.com>
2022-08-20 17:36:28 -0400

    Trying soup again

--- nofrills.py
+++ nofrills.py
@@ -1,26 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib.request
 
-URL = "https://www.nofrills.ca/search?search-bar=bananas"
-page = requests.get(URL)
+#-------------------------------------------------------------------------------------------------------------------------#
 
-soup = BeautifulSoup(page.content, "html.parser")
+# First try did not really work
 
-results = soup.find("div", class_ = "search-page__result")
+# URL = "https://www.nofrills.ca/search?search-bar=bananas"
+# page = requests.get(URL)
 
-food_elements = results.find_all("div", class_= "product-tracking")
+# soup = BeautifulSoup(page.content, "html.parser")
 
-for food in food_elements:
-    groceries = results.find_all(
-    "h2", string=lambda text: "banana" in text.lower()
-    )
+# results = soup.find(class_ = "responsive-image")
 
-    groceries_elements = [
-        h2_element.parent.parent.parent for h2_element in groceries
-    ]
-for job_element in groceries_elements:
-    # -- snip --
-    links = job_element.find_all("a")
-    for link in links:
-        link_url = job_element.find_all("a")[1]["href"]
-        print(f"Apply here: {link_url}\n")
+# food_elements = results.find_all(class_= "Bananas, Bunch")
+
+# for food in food_elements:
+#     groceries = results.find_all(
+#     "h2", string=lambda text: "banana" in text.lower()
+#     )
+
+#     groceries_elements = [
+#         h2_element.parent.parent.parent for h2_element in groceries
+#     ]
+# for job_element in groceries_elements:
+#     # -- snip --
+#     links = job_element.find_all("a")
+#     for link in links:
+#         link_url = job_element.find_all("a")[1]["href"]
+#         print(f"Apply here: {link_url}\n")
+
+# foods = [a.find('img') for a in soup.find_all("a", {"class": "product-tile__thumbnail"}) if a.find('img')]
+# name = [foods.get('alt') for food in foods]
+
+# print(name)
+
+# foods = [a.find('img').get('alt') for a in soup.find_all("a", {"class": "product-tile__thumbnail"}) if a.find('img')]
+
+# print(foods)
+
+# -------------------------------------------------------------------------------------------------------------------------#
+
+# Second try!
+
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+
+headers = {'User-Agent': user_agent, }
+
+url = 'https://www.nofrills.ca/search?search-bar=bananas'
+
+request = urllib.request.Request(url, None, headers)
+
+response = urllib.request.urlopen(request)
+
+soup = BeautifulSoup(html, "lxml")
+
+for foo in soup.find_all('img', alt=True):
+
+    print(foo['alt'])
