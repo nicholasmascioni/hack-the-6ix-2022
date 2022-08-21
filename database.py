490e7f517baca48146fdc588948f52f24e305889
jumpkid23 <111645802+jumpkid23@users.noreply.github.com>
2022-08-20 21:53:48 -0400

    Update database.py
    my bad

--- database.py
+++ database.py
@@ -2,9 +2,61 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get("https://www.nofrills.ca/search?search-bar=banana").text
-soup = BeautifulSoup(html_text, 'lxml')
 
-something = soup.find_all('div')
-print(something)
+no_frills_database = { 
+    "price_match": True,
+    "butter": 2.05,
+    "white sugar": 8.99,
+    "brown sugar": 8.99,
+    "eggs": 8.99,
+    "vanilla extract": 8.99,
+    "baking soda": 8.99,
+    "salt": 8.99,
+    "all-purpose flour": 8.99,
+    "white sugar": 8.99,
+    "semisweet chocolate chips": 8.99,
+    "chopped walnuts": 8.99
+}
 
+walmart_database = { 
+    "price_match": True,
+    "butter": 2.05,
+    "white sugar": 8.99,
+    "brown sugar": 8.99,
+    "eggs": 8.99,
+    "vanilla extract": 8.99,
+    "baking soda": 8.99,
+    "salt": 8.99,
+    "all-purpose flour": 8.99,
+    "white sugar": 8.99,
+    "semisweet chocolate chips": 8.99,
+    "chopped walnuts": 8.99
+}
+freshco_database = { 
+    "price_match": True,
+    "butter": 2.05,
+    "white sugar": 8.99,
+    "brown sugar": 8.99,
+    "eggs": 8.99,
+    "vanilla extract": 8.99,
+    "baking soda": 8.99,
+    "salt": 8.99,
+    "all-purpose flour": 8.99,
+    "white sugar": 8.99,
+    "semisweet chocolate chips": 8.99,
+    "chopped walnuts": 8.99
+}
+t_and_t_database = { 
+    "price_match": False,
+    "butter": 2.05,
+    "white sugar": 8.99,
+    "brown sugar": 8.99,
+    "eggs": 8.99,
+    "vanilla extract": 8.99,
+    "baking soda": 8.99,
+    "salt": 8.99,
+    "all-purpose flour": 8.99,
+    "white sugar": 8.99,
+    "semisweet chocolate chips": 8.99,
+    "chopped walnuts": 8.99
+}
